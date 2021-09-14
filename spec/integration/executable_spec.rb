64979b29d1a86c025436474715e6903fa0855422
Piotr Murach <pmurach@gmail.com>
2021-09-14 23:50:28 +0200

    Add integration test for game executable

---
+++ spec/integration/executable_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require "open3"
+
+RSpec.describe "executable" do
+  it "runs the game executable without an error and quits",
+     unless: RSpec::Support::OS.windows? do
+    out, err, status = Open3.capture3("minehunter -c 7", stdin_data: "q")
+
+    expect(out.inspect).to match(/ ░░░░░░░ /)
+    expect(err).to eq("")
+    expect(status.exitstatus).to eq(0)
+  end
+end
